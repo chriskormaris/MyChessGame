40365576e73c5614c7c93c88947069d03adf03f3
Christos Kormaris <chriskormaris@gmail.com>
2021-01-09 23:52:10 +0200

    Minor comment changes in `Constants.java`.

--- src/utilities/Constants.java
+++ src/utilities/Constants.java
@@ -12,10 +12,10 @@ public class Constants {
 
 	public final static int CAPTURED_PIECE_PIXEL_SIZE = 16;
 	
-	/* the default number of rows, (this should always be 8). */
+	/* the default number of rows, (this may not always be 8) */
 	public final static int DEFAULT_NUM_OF_ROWS = 8;
 	
-	/* the number of columns, (this should always be 8). */
+	/* the number of columns, (this should always be 8) */
 	public final static int NUM_OF_COLUMNS = 8;  
 	
 	public final static int NO_PIECE_CAPTURE_DRAW_HALFMOVES_LIMIT = 100;
