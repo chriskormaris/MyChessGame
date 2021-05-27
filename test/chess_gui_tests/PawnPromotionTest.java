7463b7a854939f68f731d186d6276ddf0ff76f67
Christos Kormaris <chriskormaris@gmail.com>
2021-05-28 00:45:54 +0300

    Update test class `PawnPromotionTest`

--- test/chess_gui_tests/PawnPromotionTest.java
+++ test/chess_gui_tests/PawnPromotionTest.java
@@ -18,12 +18,19 @@ class PawnPromotionTest {
 		@SuppressWarnings("unused")
 		ChessGUI cbg = new ChessGUI(title);
 		// ChessGUI.gameParameters.gameMode = GameMode.HUMAN_VS_HUMAN;
-		ChessGUI.newGameParameters.humanPlayerAllegiance = Allegiance.BLACK;
-		ChessGUI.startNewGame();
-		// ChessGUI.restoreDefaultValues();
 		
+		// ChessGUI.newGameParameters.humanPlayerAllegiance = Allegiance.BLACK;
+		// ChessGUI.startNewGame();
+		
+		// Use this FEN position, if playing as White.
 		// String fenPosition = "4k3/2P5/8/8/8/8/2p5/4K3 w - - 0 1";
-		String fenPosition = "4k3/2P5/8/8/8/8/2p5/4K3 b - - 0 1";
+		
+		// Use this FEN position, if playing as Black.
+		// String fenPosition = "4k3/2P5/8/8/8/8/2p5/4K3 b - - 0 1";
+		
+		// If you use this FEN position, you will mate if you promote the White Pawn to Knight.
+		String fenPosition = "7Q/2Ppkp2/3bpn2/8/8/8/2p5/4K3 w - - 0 1";
+		
 		ChessGUI.placePiecesToChessBoard(fenPosition);
 
 		ChessBoard.printChessBoard(ChessGUI.chessBoard.getGameBoard());
