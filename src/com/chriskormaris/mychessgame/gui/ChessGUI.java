b8fd8eb039955dc3bcd712d866e736d5ae020ca5
Christos Kormaris <chriskormaris@gmail.com>
2022-01-25 15:54:07 +0200

    Reorder methods

--- src/com/chriskormaris/mychessgame/gui/ChessGUI.java
+++ src/com/chriskormaris/mychessgame/gui/ChessGUI.java
@@ -1701,6 +1701,16 @@ public class ChessGUI {
 		}
 	}
 
+	public static void redrawChessBoard() {
+		for (int i = 0; i < gameParameters.getNumOfRows(); i++) {
+			for (int j = 0; j < NUM_OF_COLUMNS; j++) {
+				ChessPiece chessPiece = chessBoard.getGameBoard()[i][j];
+				String position = Utilities.getPositionByRowCol(i, j);
+				placePieceToPosition(position, chessPiece);
+			}
+		}
+	}
+
 	public static void placePiecesToStartingPositions() {
 		chessBoard.placePiecesToStartingPositions();
 		for (int i = 0; i < gameParameters.getNumOfRows(); i++) {
@@ -1717,16 +1727,6 @@ public class ChessGUI {
 		halfMoveGameBoards.push(halfMoveGameBoard);
 
 		setTurnMessage();
-	}
-
-	public static void redrawChessBoard() {
-		for (int i = 0; i < gameParameters.getNumOfRows(); i++) {
-			for (int j = 0; j < NUM_OF_COLUMNS; j++) {
-				ChessPiece chessPiece = chessBoard.getGameBoard()[i][j];
-				String position = Utilities.getPositionByRowCol(i, j);
-				placePieceToPosition(position, chessPiece);
-			}
-		}
 	}
 
 	public static void placePiecesToChessBoard(String fenPosition) {
