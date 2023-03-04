a08ff9768344998274592f710fdb1345118760e9
Christos Kormaris <chriskormaris@gmail.com>
2023-03-04 15:28:16 +0200

    Code maintenance

--- api/src/main/java/com/chriskormaris/mychessgame/api/exception/InvalidFenPositionException.java
+++ api/src/main/java/com/chriskormaris/mychessgame/api/exception/InvalidFenPositionException.java
@@ -3,7 +3,7 @@ package com.chriskormaris.mychessgame.api.exception;
 public class InvalidFenPositionException extends RuntimeException {
 
 	public InvalidFenPositionException(String message) {
-		super("Invalid FEN position given.\n" + message);
+		super(String.format("Invalid FEN position given.\n %s", message));
 	}
 
 }
