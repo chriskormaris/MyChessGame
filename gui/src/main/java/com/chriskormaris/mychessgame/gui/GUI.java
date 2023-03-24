79fec63823c25324e100dc018ec0f1123ccbc20d
Christos Kormaris <chriskormaris@gmail.com>
2023-03-24 14:05:29 +0200

    Code maintenance

--- gui/src/main/java/com/chriskormaris/mychessgame/gui/GUI.java
+++ gui/src/main/java/com/chriskormaris/mychessgame/gui/GUI.java
@@ -5,8 +5,7 @@ import com.chriskormaris.mychessgame.gui.drag_and_drop.DragAndDropGui;
 public class GUI {
 
 	public static void main(String[] args) {
-		DragAndDropGui dragAndDropGui = new DragAndDropGui();
-		dragAndDropGui.startNewGame();
+		DragAndDropGui.main(args);
 	}
 
 }
