03699521f1d42c29343d63d2c6dee375f946e69f
Christos Kormaris <chriskormaris@gmail.com>
2024-02-26 08:27:16 +0200

    Version 9.4.2: Add Scandinavian Defense opening

--- api/src/main/java/com/chriskormaris/mychessgame/api/ai/OpeningMoves.java
+++ api/src/main/java/com/chriskormaris/mychessgame/api/ai/OpeningMoves.java
@@ -88,6 +88,14 @@ public class OpeningMoves {
 		movesList.add(new Move("D2", "D4"));  // C00 French defence
 		movesList.add(new Move("G1", "F3"));  // C00 French defence
 		WHITE_OPENING_MOVES.put("rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2", movesList);
+
+		movesList = new ArrayList<>();
+		movesList.add(new Move("E4", "D5"));  // B01 Scandinavian (centre counter) defence
+		WHITE_OPENING_MOVES.put("rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq d6 0 2", movesList);
+
+		movesList = new ArrayList<>();
+		movesList.add(new Move("B1", "C3"));  // B01 Scandinavian (centre counter) defence
+		WHITE_OPENING_MOVES.put("rnb1kbnr/ppp1pppp/8/3q4/8/8/PPPP1PPP/RNBQKBNR w KQkq - 0 3", movesList);
 	}
 
 	// Initialize Black opening moves.
@@ -111,6 +119,7 @@ public class OpeningMoves {
 		movesList = new ArrayList<>();
 		movesList.add(new Move("D7", "D6"));  // B00 King's pawn opening
 		movesList.add(new Move("B8", "C6"));  // B00 KP, Nimzovich defence
+		movesList.add(new Move("D7", "D5"));  // B01 Scandinavian (centre counter) defence
 		movesList.add(new Move("C7", "C6"));  // B10 Caro-Kann defence
 		movesList.add(new Move("C7", "C5"));  // B20 Sicilian defence
 		movesList.add(new Move("E7", "E6"));  // C00 French defence
@@ -173,6 +182,14 @@ public class OpeningMoves {
 		movesList = new ArrayList<>();
 		movesList.add(new Move("D7", "D5"));  // C00 French defence
 		BLACK_OPENING_MOVES.put("rnbqkbnr/pppp1ppp/4p3/8/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2", movesList);
+
+		movesList = new ArrayList<>();
+		movesList.add(new Move("D8", "D5"));  // B01 Scandinavian (centre counter) defence
+		BLACK_OPENING_MOVES.put("rnbqkbnr/ppp1pppp/8/3P4/8/8/PPPP1PPP/RNBQKBNR b KQkq - 0 2", movesList);
+
+		movesList = new ArrayList<>();
+		movesList.add(new Move("D5", "D8"));  // B01 Scandinavian (centre counter) defence
+		BLACK_OPENING_MOVES.put("rnb1kbnr/ppp1pppp/8/3q4/8/2N5/PPPP1PPP/R1BQKBNR b KQkq - 1 3", movesList);
 	}
 
 
